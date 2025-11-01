e3826ebe4644cfcfcd23349da8b90aeb19975be6
nalantishantha <nalantishantha@gmail.com>
2025-11-02 01:26:33 +0530

    finish

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1.0",
+    author = "Nalan Tishantha",
+    author_email = "nalantishantha@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
